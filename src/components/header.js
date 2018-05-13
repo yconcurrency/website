7d5fd8bd1e6b8e30a4657d7e3733b810f019b953
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2018-05-13 12:54:43 +0200

    Build script creates http2push header

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ export default class Header extends Component {
       <header class="header">
         <div class="container">
           <h1 class="site-title">The Yjs Library</h1>
-          <span class="site-tagline">A small CRDT library with a powerful abstraction over shared data</span>
+          <span class="site-tagline">A CRDT library with a powerful abstraction over shared data</span>
         </div>
       </header>
     )
